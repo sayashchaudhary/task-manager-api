0601ddfc29366da8f161d2369c5450c27f2ebead
sayash chaudhary <sayashchaudhary@gmail.com>
2020-05-30 23:12:20 +0530

    creating task endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,12 +6,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true
 });
 
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
-// });
+
 
