cf27a05b8ae1b266dc0b7492dd9dc772519aa746
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-06 22:50:56 +0530

    auth middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 //Creating users
 router.post('/users', async (req, res) => {
@@ -26,7 +27,7 @@ router.post('/users/login', async (req, res) => {
 })
 
 //Fetching all users
-router.get('/users', async (req, res) => {
+router.get('/users', auth, async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users)
