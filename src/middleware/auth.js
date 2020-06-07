e23d0d861245a0b051bd4ee11e8d9c653c4dbeb2
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-07 22:59:30 +0530

    error fixed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,18 +3,24 @@ const User = require('../models/user');
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorization').replace('Bearer', '');
-        const decoded = jwt.verify(token, 'taskmanagerapi');
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+        const token = req.header('Authorization').replace('Bearer ', '');
+
+        if (!token) {
+            res.status(401).send('Invalid Token')
+        }
+
+        const tokenDecode = jwt.decode(token, "taskmanagaerapi");
+        const user = await User.findOne({ _id: tokenDecode._id, 'tokens.token': token });
+
         if (!user) {
             res.status(400).send('User not found')
-        } else {
-            req.user = user
-            next()
         }
-    } catch (e) {
-        console.log(e)
-        res.status(401).send({ error: 'Please authenticate' })
+
+        req.user = user;
+        req.token = token;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: 'Please Authenticate!' })
     }
 }
 
