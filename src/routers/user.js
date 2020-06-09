590de432b5fafa12947f0073cb71c8e07264090d
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-10 00:38:29 +0530

    handling errors

--- src/routers/user.js
+++ src/routers/user.js
@@ -113,13 +113,16 @@ const upload = multer({
     },
     fileFilter(req, file, cb) {
         if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('Please upload image'))
+            return cb(new Error('Please upload an image'))
         }
         cb(undefined, true)
     }
 })
+
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router;
