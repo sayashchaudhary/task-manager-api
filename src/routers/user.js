1eb2f91342cc407d9f38144e031aa759882d3462
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-10 00:58:38 +0530

    creating avatar for user

--- src/routers/user.js
+++ src/routers/user.js
@@ -106,8 +106,8 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 });
 
+//Uploading avatar for user
 const upload = multer({
-    dest: 'avatar',
     limits: {
         fileSize: 1000000
     },
@@ -119,10 +119,19 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
+})
+
+//Deleting avatar of user
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
 })
 
 module.exports = router;
