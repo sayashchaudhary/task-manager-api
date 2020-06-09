c27c972292c4b9c776dcc7247abe3660ce2aa3f6
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-09 19:08:16 +0530

    sorting tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -24,18 +24,28 @@ router.post('/tasks', auth, async (req, res) => {
 
 //Fetching all tasks /tasks?completed=true
 //GET /tasks?limit=10&skip=0
+//GET /tasks?sortBy=createdAt:asc or :desc
 router.get('/tasks', auth, async (req, res) => {
-    const match = {}
+    const match = {};
+    const sort = {};
+
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
         await req.user.populate({
             path: 'tasks',
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.status(200).send(req.user.tasks)
