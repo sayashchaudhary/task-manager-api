65e9c3d7f61de4847c0cc9ddc0453857e2e74961
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-09 18:30:46 +0530

    added timestamps

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -14,5 +14,9 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
-});
+}, {
+    timestamps: true
+})
+
+const Task = mongoose.model('Task', taskSchema);
 module.exports = Task;
