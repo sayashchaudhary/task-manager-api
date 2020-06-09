ee8718b2bcead3244a75a1d0db24400b46982271
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-10 00:24:26 +0530

    validating file uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -107,7 +107,16 @@ router.delete('/users/me', auth, async (req, res) => {
 });
 
 const upload = multer({
-    dest: 'avatar'
+    dest: 'avatar',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload image'))
+        }
+        cb(undefined, true)
+    }
 })
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send();
