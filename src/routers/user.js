e9d8ee33ac42379c13e79d72a52787920a4e1efb
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-09 23:51:42 +0530

    adding support for file uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = new express.Router();
+const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 
@@ -104,5 +105,12 @@ router.delete('/users/me', auth, async (req, res) => {
         res.status(500).send(e)
     }
 });
+
+const upload = multer({
+    dest: 'avatar'
+})
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send();
+})
 
 module.exports = router;
