9fb3d554b848393918c11ab0139f37e7a9f2e29b
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-10 23:24:32 +0530

    added gitignore file

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,16 +1,39 @@
 const sgMail = require('@sendgrid/mail')
 const res = require("express");
 
-const sendgridAPIKey = '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';
+const sendgridAPIKey = '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';
 sgMail.setApiKey(sendgridAPIKey);
 
 const msg = {
     to: 'learnmorewithsaysh@gmail.com',
     form: 'sayashchaudhary@gmail.com',
+    name: 'sayash',
     subject: 'this is my first creation',
     text: 'I hope this is actually gets to you'
 }
 
 
-sgMail.send(msg);
+    // sgMail.send(msg);
 
+
+    sgMail
+        .send(msg)
+        .then(() => {}, error => {
+            console.error(error);
+
+            if (error.response) {
+                console.error(error.response.body)
+            }
+        });
+
+    // (async () => {
+    //     try {
+    //         await sgMail.send(msg);
+    //     } catch (error) {
+    //         console.error(error);
+    //
+    //         if (error.response) {
+    //             console.error(error.response.body)
+    //         }
+    //     }
+    // })
